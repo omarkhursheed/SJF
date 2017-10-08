b1a2f263a4bc7e4a894066e35af5c96bd0d6a2cd
Omarkhursheed <omarkhu@gmail.com>
2017-10-08 10:23:56 +0530

    setup initial class and functions

---
+++ SJFWorker.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+class process
+{
+	int arrivalTime;
+	int CPUBursts;
+public:
+	process(int arrival, int CPU)
+	{
+		arrivalTime = arrival;
+		CPUBursts = CPU;
+	}
+};
+int main(int argc, char const *argv[])
+{
+	
+	return 0;
+}
+

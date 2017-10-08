0146b024b6d236204b4a1de5300a6a56c8552153
Omarkhursheed <omarkhu@gmail.com>
2017-10-08 21:05:45 +0530

    added function calls and vector

--- SJFWorker.cpp
+++ SJFWorker.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <cstdlib>
-
+#include <vector>
 using namespace std;
 class process
 {
 	int arrivalTime;
 	int CPUBursts;
+	int processTime;
 public:
 	process(int arrival, int CPU)
 	{
@@ -15,7 +16,34 @@ public:
 };
 int main(int argc, char const *argv[])
 {
-	
+
+	int numberOfProcesses = -1;
+	char affirm = 'y';
+	vector<process> v;
+	cout << "Enter number of processes";
+	cin >> numberOfProcesses;
+	if(numberOfProcesses <= 0)
+	{
+		cout << "Number of processes must be positive";
+		return 0;
+	}
+	for(int i = 0; i < )
+	while(affirm == 'y')
+	{
+		cout << "Enter arrival time" << endl;
+		cin >> 
+		cout << "Enter number of CPU bursts" << endl;
+		
+	}
 	return 0;
 }
 
+void preemptiveSJF()
+{
+	return;
+}
+
+void nonPreemptiveSJF()
+{
+	return;
+}

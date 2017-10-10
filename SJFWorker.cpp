5659b5f3a383d387322551ee6eef2099eb628756
Omarkhursheed <omarkhu@gmail.com>
2017-10-10 22:23:41 +0530

    added non-preemptive SJF

--- SJFWorker.cpp
+++ SJFWorker.cpp
@@ -2,25 +2,46 @@
 #include <cstdlib>
 #include <vector>
 #include <string>
+#include <algorithm>
 using namespace std;
 class process
 {
+public:
+
 	int arrivalTime;
 	int CPUBursts;
 	string processName;
-public:
 	process(string pname, int arrival, int CPU)
 	{
 		arrivalTime = arrival;
 		CPUBursts = CPU;
 		processName = pname;
 	}
+	int getArrivalTimes() const
+	{
+		return arrivalTime;
+	}
+	int getCPUBursts() const
+	{
+		return CPUBursts;	
+	}
+	string getProcessName() const
+	{
+		return processName;
+	}
+	bool operator <(process rhs)
+	{ 
+		if(arrivalTime == rhs.arrivalTime || rhs.arrivalTime < arrivalTime + CPUBursts){ return CPUBursts < rhs.CPUBursts; }return arrivalTime < rhs.arrivalTime;
+	}
 };
+void preemptiveSJF(vector<process>);
+void nonPreemptiveSJF(vector<process>);
 int main(int argc, char const *argv[])
 {
 
 	int numberOfProcesses = -1;
 	char affirm = 'y';
+	int choice;
 	vector<process> v;
 	while(affirm == 'y')
 	{
@@ -40,17 +61,48 @@ int main(int argc, char const *argv[])
 		cin >> affirm;
 		v.push_back(p);
 	}
-
+	cout << "Do you wish to use nonpreemptive SJF (1) or preemptive SJF (2)";
+	cin >> choice;
+	if (choice == 1)
+	{
+		nonPreemptiveSJF(v);
+	}
+	else if (choice == 2)
+	{
+		preemptiveSJF(v);
+	}
+	else
+	{
+		cout << "Erronous input" << endl;
+		return 0;
+	}
 
 	return 0;
 }
 
-void preemptiveSJF()
+void preemptiveSJF(vector<process> v)
 {
+	vector <process>:: iterator it; 
+	for(it = v.begin(); it != v.end(); it++)
+	{
+		return;
+	}
 	return;
 }
 
-void nonPreemptiveSJF()
+void nonPreemptiveSJF(vector<process> v)
 {
+	vector <process>:: iterator it; 
+	int arrTimes[v.size()];
+	int i = 0;
+	sort(v.begin(), v.end());
+	
+	//sort(v.begin(), v.end(), cmp);
+	cout << endl;
+	cout << "Process Name\tArrival Time\tCPU Bursts:" << endl;
+	for(it = v.begin(); it != v.end(); it++)
+	{
+		cout << it->getProcessName() << '\t' << it->getArrivalTimes() << '\t' << it->getCPUBursts() << endl;
+	}
 	return;
 }
